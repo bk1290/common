cc6d1b625c4bd928299f91724eab6d5ad3e991f4
Ben <kling.ben@gmail.com>
2020-01-12 20:32:14 -0500

    - abstracted credentials to config files - rewrote email message to parse from a file - added in some templating capabilities for the email body

--- python/emailer.py
+++ python/emailer.py
@@ -1,28 +1,56 @@
 # -*- coding: utf-8 -*-
 """
 Simple command to demonstrate email send via smtplib
+Assumes Gmail account and password are stored in a separate json file
+https://docs.python.org/3/library/email.examples.html#email-examples
 """
 
 import smtplib
+import json
+from string import Template
+from email.parser import BytesParser
+from email.parser import Parser
+from email.policy import default
+
+# Set variables
+sender_account_cfg="E:/script_configs/robo_account.json"
+email_filepath="E:/script_files/emailer_template.txt"
+attachment_fp="E:/script_files/text_attachment_test.txt"
+
+# Gmail account information is stored in a JSON file
+with open(sender_account_cfg) as json_data_file:
+    data = json.load(json_data_file)
+gmail_user = data['email_address']
+gmail_password = data['email_password']
+
+# Email template file stored in a .TXT file
+with open(email_filepath, 'rb') as email_file:
+    parsed_file=BytesParser(policy=default).parse(email_file)
+
+# perform text replacements on the email file contents
+email_template = Template(parsed_file.as_string())
+email_str = email_template.safe_substitute(
+        SENDER = gmail_user,
+        VAR1 = 'Value1',
+        VAR2 = 'Value2'
+        # add as many variables as needed...
+        )
 
 
-gmail_user = input("Enter sender email: ")
-gmail_password = input("Enter password: ")
+print(parsed_file.get_body())
 
-email_from = gmail_user
-email_to = input("Enter receiver email: ")
-email_subject = 'Test Email !'
-email_body = 'Test Email Content. \nContent on new line.'
+final_email = Parser(policy=default).parsestr(email_str)
+
+final_email.add_header('Content-Disposition', 'attachment', filename=attachment_fp)
+
 
 try:
     server_ssl = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server_ssl.ehlo()   # optional
+    server_ssl.ehlo()  
     server_ssl.login(gmail_user, gmail_password)
-    # ...send emails
-    
-    server_ssl.sendmail(email_from, email_to, email_body)
+    server_ssl.send_message(final_email)
     server_ssl.close()
-    
     print('Email Sent!')
 except:
     print('Something went wrong...')
+    
